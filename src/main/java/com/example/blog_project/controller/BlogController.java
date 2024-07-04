b2ac637fb6767e77cedf99132ac1eb23a47ea6e3
h_jinnny <nd3807@naver.com>
2024-07-04 11:13:02 +0900

    깃 설정 확인

--- src/main/java/com/example/blog_project/controller/BlogController.java
+++ src/main/java/com/example/blog_project/controller/BlogController.java
@@ -100,6 +100,7 @@ public class BlogController {
             model.addAttribute("posts", posts);
         }
 
+
         model.addAttribute("user", user);
         model.addAttribute("blogUser", blogUser);
         model.addAttribute("blog", blog);
