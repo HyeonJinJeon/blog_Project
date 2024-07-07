ce68635e3f28f5adea15bc1b25154ecedbb78af9
h_jinnny <nd3807@naver.com>
2024-07-07 14:11:17 +0900

    로그아웃 구현을 위한 토큰 블랙리스트 생성

--- src/main/java/com/example/blog_project/repository/JwtBlacklistRepository.java
+++ src/main/java/com/example/blog_project/repository/JwtBlacklistRepository.java
@@ -4,4 +4,5 @@ import com.example.blog_project.domain.JwtBlacklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JwtBlacklistRepository extends JpaRepository<JwtBlacklist, Long> {
+    boolean existsByToken(String token);
 }
--- src/main/java/com/example/blog_project/service/JwtBlackListService.java
+++ src/main/java/com/example/blog_project/service/JwtBlackListService.java
@@ -5,11 +5,21 @@ import com.example.blog_project.repository.JwtBlacklistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
-public class JwtBlackListService {
+public class JwtBlacklistService {
     private final JwtBlacklistRepository jwtBlacklistRepository;
     public void save(JwtBlacklist blacklist) {
         jwtBlacklistRepository.save(blacklist);
+    }
+    public void addToBlacklist(String token, Date expirationDate) {
+        JwtBlacklist blacklist = new JwtBlacklist(token, expirationDate);
+        jwtBlacklistRepository.save(blacklist);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return jwtBlacklistRepository.existsByToken(token);
     }
 }
