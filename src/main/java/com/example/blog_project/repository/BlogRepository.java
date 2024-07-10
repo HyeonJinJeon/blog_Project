2e2f63f6a5440e27dd691806bbee888cbe29cc69
h_jinnny <nd3807@naver.com>
2024-07-10 18:33:40 +0900

    코드 삭제

--- src/main/java/com/example/blog_project/repository/BlogRepository.java
+++ src/main/java/com/example/blog_project/repository/BlogRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    Blog findByUser(User user);
-
     Blog findByUserId(Long userId);
 }
