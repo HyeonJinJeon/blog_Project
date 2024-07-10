2f7a208613432fa8ee5845db0d1b44155c489b73
h_jinnny <nd3807@naver.com>
2024-07-10 18:39:22 +0900

    게시글 리스트 불러오는 jpa 함수 추가

--- src/main/java/com/example/blog_project/repository/PostRepository.java
+++ src/main/java/com/example/blog_project/repository/PostRepository.java
@@ -3,6 +3,10 @@ package com.example.blog_project.repository;
 import com.example.blog_project.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
     Post findByBlogIdAndId(Long blogId, Long postId);
+
+    List<Post> findBySeriesId(Long seriesId);
 }
