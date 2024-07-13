187dd85bed9391039606141f7316a1b421173954
h_jinnny <nd3807@naver.com>
2024-07-13 22:21:50 +0900

    posts 필드 추가

--- src/main/java/com/example/blog_project/domain/Tag.java
+++ src/main/java/com/example/blog_project/domain/Tag.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "Tags")
 @Getter
@@ -17,4 +20,7 @@ public class Tag {
 
     @Column(name = "name" , nullable = false)
     private String name;
+
+    @ManyToMany(mappedBy = "tagSet")
+    private Set<Post> posts = new HashSet<>();
 }
